f6583f0c3ff12fa7901227f1017fee39f201c82d
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-08-03 17:58:13 +0100

    Added features for increasing and decreasing temperature.

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -11,4 +11,14 @@ describe('Thermostat', function() {
   it('starts at 20 degrees', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+//Customer requirement #2 Increase the temp with an up function
+  it('increases in temperature with up()', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+//Customer requirement #e Decrease the temp with a down function
+  it('decreases in temperature with down()', function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
 });
