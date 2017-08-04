2c13127093740bd35411de3884499ddb04fbab09
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-08-04 14:34:15 +0100

    implemented AJAX for updating current city and temperature

--- src/interface.js
+++ src/interface.js
@@ -29,6 +29,21 @@ $(document).ready(function() {
     updateTemperature();
   })
 
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    $.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=21cf3a7f132c00aeea649a37346983c5&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
+  })
+// the code below is rather limiting fot the user as it gives only several options.
+  // $('#current-city').change(function() {
+  //   var city = $('#current-city').val();
+  //   $.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=21cf3a7f132c00aeea649a37346983c5&units=metric', function(data) {
+  //     $('#current-temperature').text(data.main.temp);
+  //   })
+  // })
+
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
